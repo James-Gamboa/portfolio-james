c823717d876bf8ebe7a3c21d9ba36c34439c3f0e
James Guevara Gamboa <89418909+James-Gamboa@users.noreply.github.com>
2024-10-18 16:37:27 -0600

    Update next.config.js to use images.remotePatterns instead of images.domains

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["i.postimg.cc"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.postimg.cc",
+        port: "",  
+        pathname: "/**", 
+      },
+    ],
   },
 };
 
