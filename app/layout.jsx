bef175ddbf95a6fd9709748b9734326613cba0c6
James <jjguevarag@gmail.com>
2024-11-07 16:22:25 -0600

    add favicon logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,23 @@
-"use client"; 
+"use client";
 
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import CustomCursor from "@/components/CustomCursor/page"; 
+import CustomCursor from "@/components/CustomCursor/page";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>James Portfolio</title>
+        <meta
+          name="description"
+          content="Welcome to my portfolio! Explore the projects and work I've done as a web developer. Discover my experience, skills, and passion for programming."
+        />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body>
         <ThemeProvider enableSystem={true} attribute="class">
-          <CustomCursor /> 
+          <CustomCursor />
           {children}
         </ThemeProvider>
       </body>
