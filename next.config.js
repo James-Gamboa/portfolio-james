273747162db636765425c11f1a20afed53d908fa
James-Gamboa <jjguevarag@gmail.com>
2025-03-19 14:33:55 -0600

    update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "i.postimg.cc",
-        port: "",  
-        pathname: "/**", 
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
