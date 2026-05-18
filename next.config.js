aaf2a6f0631262a16b4158afa8665e9cacd1735a
James-Guevara-Gamboa <jjguevarag@gmail.com>
2026-05-18 14:28:09 -0600

    Add new redirect rules in next.config.js and enhance portfolio data with JamesXP and PolkaCR projects

--- next.config.js
+++ next.config.js
@@ -8,6 +8,18 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "www.polkacr.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   async redirects() {
